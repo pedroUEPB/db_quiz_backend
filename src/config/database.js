9eb39b1aa08e2d670240d6da6c91509a1b3dae72
pedroandradee <pedro.h8237@gmail.com>
2021-12-03 20:55:05 -0300

    ajuste bd

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     host: process.env.DB_URL,
     username: 'screen37_acesso',
     password: process.env.USER_PASSWORD,
-    database: 'dbquizz',
+    database: 'screen37_db_quiz',
     define: {
         timestamps: true,
         underscored: true,
