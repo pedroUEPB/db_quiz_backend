75be8a70f7afafae6fd300d977482d64b3c91fb1
pedroandradee <pedro.h8237@gmail.com>
2021-12-07 18:26:58 -0300

    ajustes

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use("/questions", express.static(path.join(__dirname, "public/questions")));
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
 });
 
 app.use(helmet());
