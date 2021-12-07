2fc34b8089825009d74d52285e1c146c73f630f9
pedroandradee <pedro.h8237@gmail.com>
2021-12-07 13:40:36 -0300

    ajuste de limit

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
 });
 
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
