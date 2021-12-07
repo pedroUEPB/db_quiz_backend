0aa12f594539f67c4a6ec7ce8243991cec0075c3
pedroandradee <pedro.h8237@gmail.com>
2021-12-07 13:48:46 -0300

    ajuste

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
