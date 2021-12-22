9f5e2d3f70636cc41dfed81c67a526c22af190dd
pedroandradee <pedro.h8237@gmail.com>
2021-12-22 18:29:03 -0300

    ajuste cors

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use("/questions", express.static(path.join(__dirname, "public/questions")));
 });*/
 
 app.use(helmet());
+app.use(cors());
 app.use(allowCors);
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
